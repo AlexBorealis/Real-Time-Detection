ab68d9991afda358552013c10e781524324214bb
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-08-19 03:54:49 +0700

    feature: добавил скрипт preprocess.py для создания аугментированных изображений

---
+++ src/data/preprocess.py
@@ -0,0 +1,62 @@
+import os
+import gc
+from dotenv import load_dotenv
+from albumentations import (
+    ReplayCompose,
+    Resize,
+    GaussNoise,
+    CoarseDropout,
+    HorizontalFlip,
+)
+from src.utils.augmentations import process_image
+
+load_dotenv()
+
+# Paths
+PROCESSED_DIR = os.path.join(os.getenv("HOME_DIR"), "data/processed")
+
+# Creation Directories
+for split in ["train", "test", "val"]:
+    os.makedirs(os.path.join(PROCESSED_DIR, split, "images"), exist_ok=True)
+    os.makedirs(os.path.join(PROCESSED_DIR, split, "labels"), exist_ok=True)
+
+# Augmentation Pipelines
+transform = ReplayCompose(
+    [
+        Resize(height=320, width=320),
+        GaussNoise(p=0.5, std_range=(0.1, 0.1)),
+        CoarseDropout(p=0.5, num_holes_range=(1, 3), hole_height_range=(8, 32)),
+        HorizontalFlip(p=0.5),
+    ]
+)
+
+# Processing
+for split in ["train", "test", "val"]:
+    images_dir = os.path.join(os.getenv("RAW_IMAGES_DIR"), split)
+    labels_dir = os.path.join(os.getenv("RAW_LABELS_DIR"), split)
+    processed_images_dir = os.path.join(PROCESSED_DIR, split, "images")
+    processed_labels_dir = os.path.join(PROCESSED_DIR, split, "labels")
+
+    # noinspection PyRedeclaration
+    selected_img_files = os.listdir(images_dir)
+
+    batch_size_processing = 5000
+    for i in range(0, len(selected_img_files), batch_size_processing):
+        # noinspection PyRedeclaration
+        batch_files = selected_img_files[i : i + batch_size_processing]
+        for img_file in batch_files:
+            img_path = os.path.join(images_dir, img_file)
+            label_file = img_file.replace(".jpg", ".json").replace(".png", ".json")
+            label_path = os.path.join(labels_dir, label_file)
+
+            process_image(
+                img_path,
+                label_path,
+                processed_images_dir,
+                processed_labels_dir,
+                transform,
+            )
+
+        # Clearing memory after batch
+        batch_files = None
+        gc.collect()
