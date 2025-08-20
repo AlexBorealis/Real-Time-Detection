db1f7bf1a314ee07ee4a8b49763157f2927214f6
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-08-21 01:28:50 +0700

    feature: добавил Dockerfile, Makefile
    feature: добавил скрипты моделей yolo8, yolo11

---
+++ src/experiments/yolov8n.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+from ultralytics import YOLO
+
+load_dotenv()
+
+PROCESSED_DIR = os.path.join(os.getenv("HOME_DIR"), "data/processed")
+RESULTS_DIR = os.path.join(os.getenv("HOME_DIR"), "results", "models")
+MODELS_DIR = os.path.join(os.getenv("HOME_DIR"), "src", "models")
+
+model = YOLO(os.path.join(MODELS_DIR, "yolov8n.pt"), task='detect', verbose=True)
+
+results = model.train(
+    data=os.path.join(os.getenv("HOME_DIR"), 'config', "bdd100k.yaml"),
+    epochs=1,
+    imgsz=320,
+    project=RESULTS_DIR,
+    name="yolo_baseline",
+    cache=False
+)
+
+metrics = model.val()
+print(f"mAP@0.5: {metrics.box.map50}")
