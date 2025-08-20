d0413a12457b7584e7b821fe388c2a5e9de30cdb
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-08-21 01:20:24 +0700

    features: обновил preprocess.py и добавил изменение формата меток для yolo

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,24 +1,29 @@
-import os
 import gc
-from dotenv import load_dotenv
+import os
+import time
+
 from albumentations import (
+    CoarseDropout,
+    GaussNoise,
     ReplayCompose,
     Resize,
-    GaussNoise,
-    CoarseDropout,
-    HorizontalFlip,
 )
+from dotenv import load_dotenv
+
 from src.utils.augmentations import process_image
+from src.utils.utils import convert_labels
 
 load_dotenv()
+
+start = time.time()
 
 # Paths
 PROCESSED_DIR = os.path.join(os.getenv("HOME_DIR"), "data/processed")
 
 # Creation Directories
 for split in ["train", "test", "val"]:
-    os.makedirs(os.path.join(PROCESSED_DIR, split, "images"), exist_ok=True)
-    os.makedirs(os.path.join(PROCESSED_DIR, split, "labels"), exist_ok=True)
+    os.makedirs(os.path.join(PROCESSED_DIR, "images", split), exist_ok=True)
+    os.makedirs(os.path.join(PROCESSED_DIR, "labels", split), exist_ok=True)
 
 # Augmentation Pipelines
 transform = ReplayCompose(
@@ -26,7 +31,6 @@ transform = ReplayCompose(
         Resize(height=320, width=320),
         GaussNoise(p=0.5, std_range=(0.1, 0.1)),
         CoarseDropout(p=0.5, num_holes_range=(1, 3), hole_height_range=(8, 32)),
-        HorizontalFlip(p=0.5),
     ]
 )
 
@@ -34,13 +38,13 @@ transform = ReplayCompose(
 for split in ["train", "test", "val"]:
     images_dir = os.path.join(os.getenv("RAW_IMAGES_DIR"), split)
     labels_dir = os.path.join(os.getenv("RAW_LABELS_DIR"), split)
-    processed_images_dir = os.path.join(PROCESSED_DIR, split, "images")
-    processed_labels_dir = os.path.join(PROCESSED_DIR, split, "labels")
+    processed_images_dir = os.path.join(PROCESSED_DIR, "images", split)
+    processed_labels_dir = os.path.join(PROCESSED_DIR, "labels", split)
 
     # noinspection PyRedeclaration
     selected_img_files = os.listdir(images_dir)
 
-    batch_size_processing = 5000
+    batch_size_processing = 10000
     for i in range(0, len(selected_img_files), batch_size_processing):
         # noinspection PyRedeclaration
         batch_files = selected_img_files[i : i + batch_size_processing]
@@ -60,3 +64,14 @@ for split in ["train", "test", "val"]:
         # Clearing memory after batch
         batch_files = None
         gc.collect()
+
+print(f"Time {time.time() - start}")
+
+
+for split in ['train', 'val', 'test']:
+    convert_labels(
+        os.path.join(PROCESSED_DIR, 'labels', split),
+        os.path.join(PROCESSED_DIR, 'labels', split)
+    )
+
+print(f"Time {time.time() - start}")
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,7 @@
-from numpy.core.multiarray import array
-from skimage.feature import hog
-import cv2
 import json
+import os
+
+import cv2
 
 
 def load_data(img_path: str, label_path: str, rgb: bool = True):
@@ -17,11 +17,46 @@ def load_data(img_path: str, label_path: str, rgb: bool = True):
     return image, labels
 
 
-def extract_hog(img: array):
-    return hog(
-        img,
-        orientations=8,
-        pixels_per_cell=(16, 16),
-        cells_per_block=(1, 1),
-        visualize=False,
-    )
+def convert_labels(input_dir: str, output_dir: str, img_size: tuple = (320, 320)):
+    for label_file in os.listdir(input_dir):
+        if label_file.endswith(".json"):
+            img_name = label_file.replace(".json", "")
+            with open(os.path.join(input_dir, label_file), "r") as f:
+                data = json.load(f)
+            frame = data.get("frames", [{}])[0]
+            objects = frame.get("objects", [])
+            with open(os.path.join(output_dir, f"{img_name}.txt"), "w") as f_out:
+                for obj in objects:
+                    if "box2d" in obj and obj["category"] in [
+                        "person",
+                        "car",
+                        "truck",
+                        "bus",
+                        "train",
+                        "motorcycle",
+                        "bicycle",
+                        "rider",
+                    ]:
+                        class_id = [
+                            "person",
+                            "car",
+                            "truck",
+                            "bus",
+                            "train",
+                            "motorcycle",
+                            "bicycle",
+                            "rider",
+                        ].index(obj["category"])
+                        x1, y1, x2, y2 = (
+                            obj["box2d"]["x1"],
+                            obj["box2d"]["y1"],
+                            obj["box2d"]["x2"],
+                            obj["box2d"]["y2"],
+                        )
+                        x_center = (x1 + x2) / (2 * img_size[0])
+                        y_center = (y1 + y2) / (2 * img_size[1])
+                        width = (x2 - x1) / img_size[0]
+                        height = (y2 - y1) / img_size[1]
+                        f_out.write(
+                            f"{class_id} {x_center} {y_center} {width} {height}\n"
+                        )
