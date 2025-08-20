787b3ed1586e364f0b49972bdf2432999b619649
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-08-21 04:32:05 +0700

    style: обновил импорт переменных

--- src/experiments/yolov8n.py
+++ src/experiments/yolov8n.py
@@ -1,24 +1,27 @@
 import os
 
+import yaml
 from dotenv import load_dotenv
 from ultralytics import YOLO
 
 load_dotenv()
 
-MODEL_NAME = "yolov8n.pt"
-RESULTS_DIR = os.path.join(os.getenv("HOME_DIR"), "results")
-RESULTS_MODEL_NAME = "yolo_baseline"
+yaml_path = os.path.join(os.getenv("HOME_DIR"), "config", "models", "yolov8n.yaml")
+
+with open(yaml_path, "r") as file:
+    args = yaml.safe_load(file)
 
 model_path = os.path.join(
-    os.getenv("HOME_DIR"), "src", "models", "initial_models", MODEL_NAME
+    os.getenv("HOME_DIR"), "src", "models", "initial_models", args["model_name"]
 )
 model = YOLO(model_path, task="detect", verbose=True)
 
 results = model.train(
-    data=os.path.join(os.getenv("HOME_DIR"), "config", "bdd100k.yaml"),
+    data=os.path.join(os.getenv("HOME_DIR"), "config", "datasets", "bdd100k.yaml"),
     epochs=1,
     imgsz=320,
-    project=os.path.join(RESULTS_DIR, "models"),
-    name=RESULTS_MODEL_NAME,
+    project=os.path.join(os.getenv("HOME_DIR"), "results", "models"),
+    name=args["results_model_name"],
     cache=False,
+    exist_ok=True,
 )
--- src/models/test/testing_yolo_baseline.py
+++ src/models/test/testing_yolo_baseline.py
@@ -1,23 +1,43 @@
 import os
 
+import yaml
 from dotenv import load_dotenv
 from ultralytics import YOLO
 
 load_dotenv()
 
-RESULTS_DIR = os.path.join(os.getenv("HOME_DIR"), "results")
+yaml_path = os.path.join(os.getenv("HOME_DIR"), "config", "models", "yolov8n.yaml")
+
+with open(yaml_path, "r") as file:
+    args = yaml.safe_load(file)
+
 TESTING_DIR = os.path.join(os.getenv("HOME_DIR"), "data", "processed", "images", "test")
 
-model_path = os.path.join(RESULTS_DIR, "models", "yolo_baseline", "weights", "best.pt")
+model_path = os.path.join(
+    os.getenv("HOME_DIR"),
+    "results",
+    "models",
+    args["results_model_name"],
+    "weights",
+    "best.pt",
+)
 model = YOLO(model_path, task="detect", verbose=True)
 
 file_path = os.path.join(
-    RESULTS_DIR,
+    os.getenv("HOME_DIR"),
+    "results",
     "visualizations",
-    "yolo_baseline",
+    args["results_model_name"],
 )
 prediction = model.predict(
-    source=os.path.join(TESTING_DIR, "cad7fdff-d9946f73.jpg"),
+    source=os.path.join(
+        os.getenv("HOME_DIR"),
+        "data",
+        "processed",
+        "images",
+        "test",
+        "cad7fdff-d9946f73.jpg",
+    ),
     conf=0.25,
     iou=0.7,
     save=True,
@@ -25,4 +45,3 @@ prediction = model.predict(
     project=file_path,
     exist_ok=True,
 )
-
