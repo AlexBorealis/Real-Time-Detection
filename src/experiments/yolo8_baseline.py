911541aaa8ff40dab0e847cb67fa086554a65bfe
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-08-21 15:33:22 +0700

    feature: переименовал скрипты и файлы конфига для моделей

---
+++ src/experiments/yolo8_baseline.py
@@ -0,0 +1,56 @@
+import os
+
+import yaml
+from dotenv import load_dotenv
+from ultralytics import YOLO
+
+from src.utils.utils import convert_labels
+
+load_dotenv()
+
+# Set directory
+os.chdir(os.getenv("HOME_DIR"))
+
+# Set model.yaml path
+yaml_path = os.path.join(
+    os.getenv("HOME_DIR"), "config", "models", "yolo8_baseline.yaml"
+)
+with open(yaml_path, "r") as file:
+    args = yaml.safe_load(file)
+
+# Set directories path for training
+PROCESSED_DIR = os.path.join(os.getenv("HOME_DIR"), "data", "processed")
+PROJECT_DIR = os.path.join(
+    os.getenv("HOME_DIR"), "results", "models", args["results_model_name"]
+)
+DATA_DIR = os.path.join(os.getenv("HOME_DIR"), "config", "datasets", "bdd100k.yaml")
+
+# Modify labels from .json to .txt
+for split in ["train", "val", "test"]:
+    convert_labels(
+        os.path.join(PROCESSED_DIR, "labels", split),
+        os.path.join(PROCESSED_DIR, "labels", split),
+        args["selected_classes"],
+    )
+
+# Get model
+model_path = os.path.join(
+    os.getenv("HOME_DIR"), "src", "models", "initial_models", args["model_name"]
+)
+model = YOLO(model_path, task="detect", verbose=True)
+
+# Train
+results = model.train(
+    data=DATA_DIR,
+    project=PROJECT_DIR,
+    epochs=100,
+    imgsz=320,
+    exist_ok=True,
+    device=-1,
+    patience=10,
+    batch=8,
+    optimizer="AdamW",
+    classes=[0, 1, 5, 6, 7],
+    cos_lr=True,
+    plots=True,
+)
