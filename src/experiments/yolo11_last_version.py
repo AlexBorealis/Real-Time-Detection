5db049a6cdc212055657dc0477f6f9c97804abab
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-08-22 16:01:06 +0700

    feature, style: модифицирован код для yolo11

--- src/experiments/yolo11_last_version.py
+++ src/experiments/yolo11_last_version.py
@@ -8,35 +8,47 @@ from src.utils.utils import convert_labels
 
 load_dotenv()
 
-PROCESSED_DIR = os.path.join(os.getenv("HOME_DIR"), "data", "processed")
+# Set directory
+os.chdir(os.getenv("HOME_DIR"))
+
+# Set model.yaml path
 yaml_path = os.path.join(
     os.getenv("HOME_DIR"), "config", "models", "yolo11_last_version.yaml"
 )
-
 with open(yaml_path, "r") as file:
     args = yaml.safe_load(file)
+
+# Set directories path for training
+PROCESSED_DIR = os.path.join(os.getenv("HOME_DIR"), "data", "processed")
+PROJECT_DIR = os.path.join(
+    os.getenv("HOME_DIR"), "results", "models", args["project_results_name"]
+)
+DATA_DIR = os.path.join(os.getenv("HOME_DIR"), "config", "datasets", "bdd100k.yaml")
+IMG_SIZE = int(os.getenv("HEIGHT")), int(os.getenv("WIDTH"))
+
+# Modify labels from .json to .txt
 for split in ["train", "val", "test"]:
     convert_labels(
         os.path.join(PROCESSED_DIR, "labels", split),
         os.path.join(PROCESSED_DIR, "labels", split),
         args["selected_classes"],
+        img_size=IMG_SIZE
     )
 
+# Get model
 model = YOLO(args["model_name"], task="detect", verbose=True)
 
+# Train
 results = model.train(
-    data=os.path.join(os.getenv("HOME_DIR"), "config", "datasets", "bdd100k.yaml"),
-    project=os.path.join(os.getenv("HOME_DIR"), "results", "models"),
+    data=DATA_DIR,
+    project=PROJECT_DIR,
     epochs=100,
-    imgsz=320,
-    cache=True,
+    imgsz=IMG_SIZE[0],
+    batch=8,
     exist_ok=True,
     device=-1,
-    resume=True,
-    patience=5,
-    batch=-1,
+    patience=10,
     optimizer="AdamW",
-    classes=[0, 1, 5, 6, 7],
-    cos_lr=True,
     plots=True,
+    amp=False,
 )
